8d4598892b658e2b753b13f8f854aa753fb57697
Salamandra6 <phantombadger6@gmail.com>
2026-07-21 09:54:39 -0400

    Integra aviso de respaldo automático CPU

--- audio2text/advanced_app.py
+++ audio2text/advanced_app.py
@@ -2,12 +2,14 @@ from __future__ import annotations
 
 from .advanced_panel_mixin import AdvancedPanelMixin
 from .advanced_processing_mixin import AdvancedProcessingMixin
+from .cuda_fallback_notice_mixin import CudaFallbackNoticeMixin
 from .productivity_app import Audio2TextApp as ProductivityAudio2TextApp
 from .version_notice_mixin import VersionNoticeMixin
 
 
 class Audio2TextApp(
     VersionNoticeMixin,
+    CudaFallbackNoticeMixin,
     AdvancedPanelMixin,
     AdvancedProcessingMixin,
     ProductivityAudio2TextApp,
